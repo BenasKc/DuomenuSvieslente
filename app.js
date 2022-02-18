c26bacc181e2d0d0da6a01392f80f67cd5749265
BenasKc <benas.kucinskas.org@gmail.com>
2022-02-18 16:44:58 +0200

    Node.js ir Vue.js

---
+++ app.js
@@ -0,0 +1,4 @@
+const app = Vue.createApp({
+    template: "<h1>Hello world</h1>",
+});
+app.mount('#app');
--- index.js
+++ index.js
@@ -1,8 +1,28 @@
 const http = require("http");
 const fs = require("fs");
 
-http.createServer((req,res)=>{
-    res.writeHead(200, { 'Content-Type':'text/html'});
-    res.write("Hello world!");
-    res.end();
+function sendFile(filename, contentType, res, callback){
+    fs.readFile(filename, (err,data) => {
+        if(err){
+            console.log(err);
+            res.write(err);
+            return;
+        }
+        else{
+            res.writeHead(200, {'Content-Type':contentType});
+            res.write(data);      
+        }
+        res.end();
+        if(callback){
+            callback(error);
+        }
+    });
+}
+const server = http.createServer((req,res)=>{
+    if(req.url === "/app.js"){
+        sendFile('app.js', 'text/javascript', res);
+    }
+    if(req.url === "/"){
+        sendFile('index.html', 'text/html', res);
+    }
 }).listen(8080);
