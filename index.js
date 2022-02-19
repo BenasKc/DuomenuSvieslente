ea40a990cdcee3884600d04e0369fc6e62e10988
BenasKc <benas.kucinskas.org@gmail.com>
2022-02-19 17:18:09 +0200

    Visas auth be API

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const http = require("http");
 const fs = require("fs");
+const cookie = require('cookie');
 
 function sendFile(filename, contentType, res, callback){
     fs.readFile(filename, (err,data) => {
@@ -19,28 +20,51 @@ function sendFile(filename, contentType, res, callback){
     });
 }
 const server = http.createServer((req,res)=>{
-    if(req.url === "/app.js"){
-        sendFile('app.js', 'text/javascript', res);
+    var items = req.rawHeaders;
+    var login = false;
+    for(i = 0;i < items.length;i++){
+        if(items[i]=='Cookie'){
+            var items2 = items[i+1].split(';');
+            if(items2[0]==='true' && items2[1] === ' login=true')login=true;
+            else login = false;
+            break;
+        }
     }
-    if(req.url === "/login.js"){
-        sendFile('login.js', 'text/javascript', res);
+    if(login === false){
+        if(req.url === "/login.js"){
+            sendFile('login.js', 'text/javascript', res);
+        }
+        else{
+            sendFile('login.html', 'text/html', res);
+        }
     }
-    if(req.url === "/"){
-        res.writeHead(302, {
-            'Location':'/login'
-        });
-        res.end();
+    else{
+        if(req.url === "/login"){
+            res.writeHead(302, {
+                'Location':'/index'
+            });
+            res.end();
+        }
+        if(req.url === "/app.js"){
+            sendFile('app.js', 'text/javascript', res);
+        }
+        if(req.url === "/"){
+            res.writeHead(302, {
+                'Location':'/index'
+            });
+            res.end();
+        }
+        if(req.url === '/index'){
+            sendFile('index.html', 'text/html', res);
+        }
+        
+        if(req.url === '/profile'){
+            sendFile('profile.html', 'text/html', res);
+        }
+        if(req.url === '/draw.js'){
+            sendFile('draw.js', 'text/javascript', res);
+        }
     }
-    if(req.url === '/index'){
-        sendFile('index.html', 'text/html', res);
-    }
-    if(req.url === '/login'){
-        sendFile('login.html', 'text/html', res);
-    }
-    if(req.url === '/profile'){
-        sendFile('profile.html', 'text/html', res);
-    }
-    if(req.url === '/draw.js'){
-        sendFile('draw.js', 'text/javascript', res);
-    }
+    
+    
 }).listen(8080);
--- login.js
+++ login.js
@@ -8,7 +8,27 @@ const app = Vue.createApp({
     },
     methods:{
         pass:function(event){
-            console.log(this.username + ' ' + this.password);
+            if(this.username === 'BenasKc' && this.password==="Benas123")
+            {
+                alert("Login successful");
+                const d = new Date();
+                d.setTime(d.getTime() + (15*60*1000));
+                let expires = "expires="+ d.toUTCString();
+                document.location.reload(true);
+                document.cookie = `login=true; expires=${expires};`;
+            }
+            else if(this.username === 'silumini' && this.password === "Arnas123"){
+                alert("Login successful");
+                const d = new Date();
+                d.setTime(d.getTime() + (15*60*1000));
+                let expires = "expires="+ d.toUTCString();
+                document.location.reload(true);
+                document.cookie = `login=true;expires=${expires};`;
+            }
+            else{
+                alert("Login failed!");
+                document.cookie = `login=false;`;
+            }
         }
     }
 })
